73d27541b447c60ea1aa4049266abafec550902a
administrador <thamycambe@gmail.com>
2023-09-27 12:17:28 -0300

    alterações de tags do html para deixar mais semântico, inclusão dos formulários dos botões, arquivo index.js inserido e estilização dos formulários no css.

---
+++ index.js
@@ -0,0 +1,16 @@
+const showLoginBtn = document.getElementById("showLoginBtn");
+const showCadastroBtn = document.getElementById("showCadastroBtn");
+const loginForm = document.getElementById("loginForm");
+const cadastroForm = document.getElementById("cadastroForm");
+
+showLoginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  loginForm.classList.add("oculto");
+  loginForm.classList.remove("oculto");
+});
+
+showCadastroBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  cadastroForm.classList.add("oculto");
+  cadastroForm.classList.remove("oculto");
+});
