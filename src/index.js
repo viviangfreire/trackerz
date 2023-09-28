4db8e1e4164f79df54d7e8425f1c1b680435b429
tamila <thamycambe@gmail.com>
2023-09-28 20:50:33 -0300

    testando menu responsivo

--- src/index.js
+++ src/index.js
@@ -14,3 +14,12 @@ showCadastroBtn.addEventListener("click", (e) => {
   cadastroForm.classList.add("oculto");
   cadastroForm.classList.remove("oculto");
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  const openMenuButton = document.getElementById('openMenuButton');
+  const mobileMenu = document.getElementById('mobileMenu');
+
+  openMenuButton.addEventListener('click', function() {
+      mobileMenu.classList.toggle('hidden');
+  });
+});
